01784f89579196cda81438db0e730b3008bc7198
Aaron Vogt <atvogt@localhost.localdomain>
2016-09-18 16:35:32 -0400

    Added HTML5 validation to Edit Contacts form.

--- app/templates/contacts/contactBase.php
+++ app/templates/contacts/contactBase.php
@@ -26,32 +26,41 @@
 
   <div id="add_contact" style="display: none">
      <p>{{ current_url }}</p>
+
   </div>
   <div id="edit_contact" style="display: none">
-      <div class="container">
-         <div class="row">
-            <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
-            <label for="tags">Enter Contact:</label>
-            <input id="tags" placeholder="Search Contacts..." />
-            <label for="edit_first_name">First Name:</label>
-            <input id="edit_first_name" placeholder="First Name..." />
-            <label for="edit_last_name">Last Name:</label>
-            <input id="edit_last_name" placeholder="Last Name..." />
-            </div>
-            <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
-               <label for="edit_phone">Phone Number:</label>
-               <input id="edit_phone" placeholder="Phone Number..." />
-               <label for="edit_email">Email:</label>
-               <input id="edit_email" placeholder="Email Address..." />
-               <label for="edit_company">Company:</label>
-               <input id="edit_company" placeholder="Company..." />
-               <div id="edit_contact_btn_div">
-                  <button id="edit_cust_submit" class="btn btn-success" type="button">Edit Contact</button>
-                  <button id="edit_cust_delete" class="btn btn-danger" type="button">Delete Contact</button>
-               </div>
-          </div>
-         </div>
-      </div>
+     <div class="container">
+        <div class="row">
+         <form>
+           <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+           <label for="tags">Enter Contact:</label>
+           <input id="tags" placeholder="Search Contacts..." />
+
+             <label for="edit_first_name">First Name:</label>
+             <input id="edit_first_name" placeholder="First Name..." required/>
+             <label for="edit_last_name">Last Name:</label>
+             <input id="edit_last_name" placeholder="Last Name..." required/>
+           </div>  <!-- END COLUMN LEFT -->
+             <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+                 <label for="edit_phone">Phone Number:</label>
+                 <input id="edit_phone" placeholder="Phone Number..."
+                     pattern="^(\([0-9]{3}\)|[0-9]{3}-)[0-9]{3}-[0-9]{4}$"
+                     title="Please enter in format xxx-xxx-xxxx" required/>
+                 <label for="edit_email">Email:</label>
+                 <input id="edit_email" placeholder="Email Address..."
+                     pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+                     title="Please enter valid Email." required/>
+                 <label for="edit_company">Company:</label>
+                 <input id="edit_company" placeholder="Company..." />
+
+                 <div id="edit_contact_btn_div">
+                 <button id="edit_cust_submit" class="btn btn-success">Edit Contact</button>
+                 <button id="edit_cust_delete" class="btn btn-danger">Delete Contact</button>
+                 </div>
+             </div> <!-- END COLUMN RIGHT -->
+           </form>
+      </div> <!-- END ROW -->
+    </div>  <!-- END CONTAINER -->
  </div>
   <div id="add_company" style="display: none"></div>
   <div id="edit_company" style="display: none"></div>
